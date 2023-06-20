08464f9d3c06975c5ebeec8cf664f5f6f079a1da
Charlie Morris <chtmorris@users.noreply.github.com>
2023-06-20 17:20:08 -0400

    Styling changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,11 +13,11 @@
 
 <main class="flex flex-col p-10 w-screen h-screen items-center gap-5 font-serif">
 
-	<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+	<nav class="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
 		<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 			<div class="flex items-center">
 				<img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Flowbite Logo">
-				<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Svelte Ethers Template</span>
+				<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white text-black">Svelte Ethers Template</span>
 			</div>
 			<div class="flex md:order-2">
 				<button on:click={connectWalletConnect} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Connect Wallet</button>
@@ -27,7 +27,7 @@
 				</button>
 			</div>
 			<div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-				<ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+				<ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 					<li>
 						<a href="#" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
 					</li>
@@ -46,7 +46,7 @@
 
 	<p>A template of useful components for Svelte Kit + Ethers + Wallet Connect</p>
 
-	<section class="flex flex-col p-10 items-start gap-5 border-2 rounded-xl mt-20">
+	<section class="flex flex-col p-10 items-start gap-5 border-2 rounded-xl mt-10 max-w-2xl">
 		<h1 class="text-2xl text-black font-bold">Wallet and Provider</h1>
 		<h2 class="text-xl text-black font-semibold">$connected: {$connected}</h2>
 		<h2 class="text-xl text-black font-semibold">$walletAddress: {$walletAddress}</h2>
@@ -69,7 +69,8 @@
 			>Disconnect</button
 		>
 	</section>
-	<section class="flex flex-col p-10 items-start gap-5 border-2 rounded-xl">
+
+	<section class="flex flex-col p-10 items-start gap-5 border-2 rounded-xl mt-10 max-w-2xl">
 		<h1 class="text-2xl text-black font-bold">Sync contract state on block, event or interval</h1>
 		<p class="text-md text-black font-semibold">
 			Set In '$lib/globalState.ts', will load as long as a store is used from '$lib/stores/state.ts'
@@ -80,4 +81,26 @@
 		</h2>
 	</section>
 
+	<section class="flex flex-col p-10 items-start gap-5 border-2 rounded-xl mt-10 max-w-2xl">
+		
+		<form>
+			<div class="contract-address relative z-0 w-full mb-6 group">
+				<input type="email" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+				<label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Contract address</label>
+			</div>
+			<button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+		</form>
+  
+	</section>
+
+	<section class="flex flex-col p-10 items-start min-w-full">
+		<p>Original template by @0xjimmy. Enhanced by @chtmorris. </p>
+	</section>
+
 </main>
+
+<style>
+	.contract-address {
+		min-width: 590px;
+	}
+</style>
